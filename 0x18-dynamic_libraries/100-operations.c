44c384a603334b57b10ee731fa92d66cb485342a
shadrack <phoenixnova427@gmail.com>
2024-09-27 17:21:31 +0300

    added 100 files

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,25 @@
+int add(int a, int b)
+{
+	int c;
+	return(c = a + b);
+}
+int sub(int a, int b)
+{
+        int c;
+        return(c = a - b);
+}
+int mul(int a, int b)
+{
+        int c;
+        return(c = a * b);
+}
+int div(int a, int b)
+{
+        int c;
+        return(c = a / b);
+}
+int mod(int a, int b)
+{
+	int c;
+	return(c = a % b);
+}
